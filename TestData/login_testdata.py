0fec91c997da59ec03b2dd63a18d454d873f2a81
Thoa Nguyen <kimthoa120494@gmail.com>
2025-11-25 22:55:14 +0700

    commit sause demo: test login function

---
+++ TestData/login_testdata.py
@@ -0,0 +1,8 @@
+class LoginTestData:
+    VALID_USERNAME = "standard_user"
+    VALID_PASSWORD = "secret_sauce"
+
+    INVALID_USERNAME = "invalidusername"
+    INVALID_PASSWORD = "abc"
+
+    ERROR_INVALID_CREDENTIALS = "Username and password do not match any user"
--- Tests/test_login_page.py
+++ Tests/test_login_page.py
@@ -1,21 +1,22 @@
 from Pages.login_page import LoginPage
+from TestData.login_testdata import LoginTestData as TD
 
 def test_login_successful(driver):
     page = LoginPage(driver)
     page.open(page.URL)
-    page.login("standard_user","secret_sauce")
+    page.login(TD.VALID_USERNAME,TD.VALID_PASSWORD)
     assert "inventory" in driver.current_url
 
 def test_login_with_invalid_username(driver):
     page = LoginPage(driver)
     page.open(page.URL)
-    page.login("invalidusername","secret_sauce")
+    page.login(TD.INVALID_USERNAME,TD.VALID_PASSWORD)
     error_message = page.get_error_message()
-    assert page.ERROR_INVALID_CREDENTIALS in error_message
+    assert TD.ERROR_INVALID_CREDENTIALS in error_message
 
 def test_login_with_invalid_password(driver):
     page = LoginPage(driver)
     page.open(page.URL)
-    page.login("standard_user","abc")
+    page.login(TD.VALID_USERNAME,TD.INVALID_PASSWORD)
     error_message = page.get_error_message()
-    assert page.ERROR_INVALID_CREDENTIALS in error_message
+    assert TD.ERROR_INVALID_CREDENTIALS in error_message
